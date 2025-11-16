1d9219322fa59611c0818e5526836b5265ef8daf
Rittik <rittikgoyal22@gmail.com>
2025-11-17 01:16:42 +0530

    added get all locations API

--- src/main/java/com/etd/travel_planner/controller/LocationController.java
+++ src/main/java/com/etd/travel_planner/controller/LocationController.java
@@ -1,12 +1,33 @@
 package com.etd.travel_planner.controller;
 
+import com.etd.travel_planner.dto.LocationResponseDTO;
+import com.etd.travel_planner.service.interfaces.LocationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/travelrequests/locations")
 public class LocationController {
 
     private final Logger logger = LoggerFactory.getLogger(LocationController.class);
+    private final LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<LocationResponseDTO>> getAllLocations() {
+        logger.info("Inside LocationController :: Fetching all locations");
+        List<LocationResponseDTO> locations = locationService.getAllLocations();
+        return new ResponseEntity<>(locations, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/etd/travel_planner/service/classes/LocationServiceImpl.java
+++ src/main/java/com/etd/travel_planner/service/classes/LocationServiceImpl.java
@@ -1,13 +1,32 @@
 package com.etd.travel_planner.service.classes;
 
+import com.etd.travel_planner.dao.LocationRepo;
+import com.etd.travel_planner.dto.LocationResponseDTO;
+import com.etd.travel_planner.entity.Location;
+import com.etd.travel_planner.mapper.LocationMapper;
 import com.etd.travel_planner.service.interfaces.LocationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LocationServiceImpl implements LocationService {
 
     private final Logger logger = LoggerFactory.getLogger(LocationServiceImpl.class);
+    private final LocationRepo locationRepo;
+    private final LocationMapper locationMapper;
 
+    public LocationServiceImpl(LocationRepo locationRepo, LocationMapper locationMapper) {
+        this.locationRepo = locationRepo;
+        this.locationMapper = locationMapper;
+    }
+
+    @Override
+    public List<LocationResponseDTO> getAllLocations() {
+        logger.info("Inside LocationServiceImpl :: Fetching all locations from the database");
+        List<Location> locations = locationRepo.findAll();
+        return locationMapper.mapLocationToLocationResponseDTO(locations);
+    }
 }
